6b7afe443f7c6da37b3f0ae084e366d5338f8259
Jay Tailor <tailorj64@gmail.com>
2023-10-03 18:59:26 +0530

    graphql setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { ApolloDriver } from '@nestjs/apollo';
+import { AppResolver } from './app/app.resolver';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot({
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    playground: false,
+    plugins: [ ApolloServerPluginLandingPageLocalDefault()],
+    driver: ApolloDriver,
+    context: ({req}) => ({ headers: req.headers})
+  })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
